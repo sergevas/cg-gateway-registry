8ae4ed4eeec470093753a89c2d3ecb92387a03b7
sergevas <brighterr@gmail.com>
2022-10-31 23:39:44 +0300

    Initial commit

---
+++ src/main/java/dev/sergevas/cg/entity/DeviceCurrentState.java
@@ -0,0 +1,51 @@
+package dev.sergevas.cg.entity;
+
+import javax.persistence.Entity;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Entity
+public class DeviceCurrentState extends BaseEntity {
+
+    private String deviceId;
+    private String deviceState;
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getDeviceState() {
+        return deviceState;
+    }
+
+    public void setDeviceState(String deviceState) {
+        this.deviceState = deviceState;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        DeviceCurrentState that = (DeviceCurrentState) o;
+        return Objects.equals(deviceId, that.deviceId) && Objects.equals(deviceState, that.deviceState);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), deviceId, deviceState);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", DeviceCurrentState.class.getSimpleName() + "[", "]")
+                .add("id='" + super.getId() + "'")
+                .add("deviceId='" + deviceId + "'")
+                .add("deviceState='" + deviceState + "'")
+                .toString();
+    }
+}
---
+++ src/main/java/dev/sergevas/cg/entity/DeviceRegistration.java
@@ -0,0 +1,66 @@
+package dev.sergevas.cg.entity;
+
+import javax.persistence.Entity;
+import java.util.StringJoiner;
+
+@Entity
+public class DeviceRegistration extends  BaseEntity {
+
+    private String deviceId;
+    private String deviceType;
+    private String deviceTags;
+    private String deviceUrl;
+    private Long statusUpdatePeriod;
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getDeviceTags() {
+        return deviceTags;
+    }
+
+    public void setDeviceTags(String deviceTags) {
+        this.deviceTags = deviceTags;
+    }
+
+    public String getDeviceUrl() {
+        return deviceUrl;
+    }
+
+    public void setDeviceUrl(String deviceUrl) {
+        this.deviceUrl = deviceUrl;
+    }
+
+    public Long getStatusUpdatePeriod() {
+        return statusUpdatePeriod;
+    }
+
+    public void setStatusUpdatePeriod(Long statusUpdatePeriod) {
+        this.statusUpdatePeriod = statusUpdatePeriod;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", DeviceRegistration.class.getSimpleName() + "[", "]")
+                .add("id=" + super.getId() + "'")
+                .add("deviceId='" + deviceId + "'")
+                .add("deviceType='" + deviceType + "'")
+                .add("deviceTags='" + deviceTags + "'")
+                .add("deviceUrl='" + deviceUrl + "'")
+                .add("statusUpdatePeriod=" + statusUpdatePeriod)
+                .toString();
+    }
+}
