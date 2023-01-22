cbead2ea04c123723d83b55c962158fc80b55dfa
sergevas <brighterr@gmail.com>
2023-01-22 23:27:54 +0300

    In progress

--- src/main/java/dev/sergevas/cg/gateway/CgGatewayApp.java
+++ src/main/java/dev/sergevas/cg/gateway/CgGatewayApp.java
@@ -1,8 +1,26 @@
 package dev.sergevas.cg.gateway;
 
+import cloud.piranha.embedded.EmbeddedPiranha;
+import cloud.piranha.embedded.EmbeddedPiranhaBuilder;
+import cloud.piranha.http.impl.DefaultHttpServer;
+import cloud.piranha.http.webapp.HttpWebApplicationServerProcessor;
+import dev.sergevas.cg.gateway.registry.adapter.in.web.api.RegistrationApi;
+
 public class CgGatewayApp {
 
     public static void main(String[] args) {
+        EmbeddedPiranha piranha = new EmbeddedPiranhaBuilder()
+                .servlet("RegistrationApi", RegistrationApi.class)
+                .servletMapping("RegistrationApi", "registry/devices/*")
+                .buildAndStart();
+
+        DefaultHttpServer defaultHttpServer = new DefaultHttpServer(8080,
+                new HttpWebApplicationServerProcessor(piranha), false);
+
+//        NettyHttpServer nettyHttpServer = new NettyHttpServer(8081,
+//                new HttpWebApplicationServerProcessor(piranha));
+
+        defaultHttpServer.start();
 
     }
 }
--- src/main/java/dev/sergevas/cg/gateway/registry/adapter/in/web/api/RegistrationApi.java
+++ src/main/java/dev/sergevas/cg/gateway/registry/adapter/in/web/api/RegistrationApi.java
@@ -1,89 +1,98 @@
 package dev.sergevas.cg.gateway.registry.adapter.in.web.api;
 
-import dev.sergevas.cg.gateway.registry.application.port.in.*;
-import dev.sergevas.cg.gateway.registry.domain.DeviceRegistration;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriInfo;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-import java.util.List;
-
-@Path("registry/devices")
-public class RegistrationApi {
-
-    @Inject
-    private GetRegisteredDeviceQuery getRegisteredDeviceQuery;
-    @Inject
-    private ToDeviceRegistrationTypeMapper toDeviceRegistrationTypeMapper;
-    @Inject
-    private RegisterDeviceUseCase registerDeviceUseCase;
-    @Inject
-    private UpdateDeviceRegistrationUseCase updateDeviceRegistrationUseCase;
-    @Inject
-    private DeleteReisteredDevicesUseCase deleteReisteredDevicesUseCase;
-    @Inject
-    UriInfo uriInfo;
-
-    @GET
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response getRegisteredDevices() {
-        List<DeviceRegistration> deviceRegistrations = getRegisteredDeviceQuery.getDevice();
-        List<DeviceRegistrationType> deviceRegistrationTypes = deviceRegistrations
-                .stream()
-                .map(dr -> toDeviceRegistrationTypeMapper.map(dr, uriInfo))
-                .toList();
-        return Response.ok().entity(deviceRegistrationTypes).build();
+//@Path("registry/devices")
+public class RegistrationApi extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
     }
 
-    @GET
-    @Path("{deviceId}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response getRegisteredDeviceInfo(@PathParam("deviceId") String deviceId) {
-        DeviceRegistrationType deviceRegistrationType = toDeviceRegistrationTypeMapper
-                .map(getRegisteredDeviceQuery.getDevice(new GetRegisteredDeviceCommand(deviceId)), uriInfo);
-        return Response.ok().entity(deviceRegistrationType).build();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
     }
 
-    @POST
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response registerDevice(DeviceRegistrationType deviceRegistrationType) {
-        RegisterDeviceCommand registerDeviceCommand = new RegisterDeviceCommand(
-                deviceRegistrationType.getDeviceId(),
-                deviceRegistrationType.getDeviceType(),
-                deviceRegistrationType.getDeviceUri(),
-                deviceRegistrationType.getStatusUpdatePeriod(),
-                deviceRegistrationType.getDeviceTags());
-        DeviceRegistrationType registered = toDeviceRegistrationTypeMapper
-                .map(registerDeviceUseCase.register(registerDeviceCommand), uriInfo);
-        return Response.ok().entity(registered).build();
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
     }
 
-    @PUT
-    @Path("{deviceId}")
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response updateRegisteredDeviceInfo(@PathParam("deviceId") String deviceId,
-                                               DeviceRegistrationType deviceRegistrationType) {
-        UpdateDeviceRegistrationCommand updateDeviceRegistrationCommand = new UpdateDeviceRegistrationCommand(
-                deviceId,
-                deviceRegistrationType.getDeviceId(),
-                deviceRegistrationType.getDeviceType(),
-                deviceRegistrationType.getDeviceUri(),
-                deviceRegistrationType.getStatusUpdatePeriod(),
-                deviceRegistrationType.getDeviceTags());
-        DeviceRegistrationType updated = toDeviceRegistrationTypeMapper
-                .map(updateDeviceRegistrationUseCase.updateDeviceRegistration(updateDeviceRegistrationCommand), uriInfo);
-        return Response.ok().entity(updated).build();
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
     }
 
-    @DELETE
-    @Path("{deviceId}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response deleteRegisteredDevice(@PathParam("deviceId") String deviceId) {
-        deleteReisteredDevicesUseCase.deleteRegisteredDevice(new DeleteRegisteredDeviceCommand(deviceId));
-        return Response.ok().build();
-    }
+//    @Inject
+//    private GetRegisteredDeviceQuery getRegisteredDeviceQuery;
+//    @Inject
+//    private ToDeviceRegistrationTypeMapper toDeviceRegistrationTypeMapper;
+//    @Inject
+//    private RegisterDeviceUseCase registerDeviceUseCase;
+//    @Inject
+//    private UpdateDeviceRegistrationUseCase updateDeviceRegistrationUseCase;
+//    @Inject
+//    private DeleteReisteredDevicesUseCase deleteReisteredDevicesUseCase;
+//    @Inject
+//    UriInfo uriInfo;
+
+//    @GET
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public Response getRegisteredDevices() {
+//        List<DeviceRegistration> deviceRegistrations = getRegisteredDeviceQuery.getDevice();
+//        List<DeviceRegistrationType> deviceRegistrationTypes = deviceRegistrations
+//                .stream()
+//                .map(dr -> toDeviceRegistrationTypeMapper.map(dr, uriInfo))
+//                .toList();
+//        return Response.ok().entity(deviceRegistrationTypes).build();
+//    }
+//
+//    @GET
+//    @Path("{deviceId}")
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public Response getRegisteredDeviceInfo(@PathParam("deviceId") String deviceId) {
+//        DeviceRegistrationType deviceRegistrationType = toDeviceRegistrationTypeMapper
+//                .map(getRegisteredDeviceQuery.getDevice(new GetRegisteredDeviceCommand(deviceId)), uriInfo);
+//        return Response.ok().entity(deviceRegistrationType).build();
+//    }
+//
+//    @POST
+//    @Consumes({MediaType.APPLICATION_JSON})
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public Response registerDevice(DeviceRegistrationType deviceRegistrationType) {
+//        RegisterDeviceCommand registerDeviceCommand = new RegisterDeviceCommand(
+//                deviceRegistrationType.getDeviceId(),
+//                deviceRegistrationType.getDeviceType(),
+//                deviceRegistrationType.getDeviceUri(),
+//                deviceRegistrationType.getStatusUpdatePeriod(),
+//                deviceRegistrationType.getDeviceTags());
+//        DeviceRegistrationType registered = toDeviceRegistrationTypeMapper
+//                .map(registerDeviceUseCase.register(registerDeviceCommand), uriInfo);
+//        return Response.ok().entity(registered).build();
+//    }
+//
+//    @PUT
+//    @Path("{deviceId}")
+//    @Consumes({MediaType.APPLICATION_JSON})
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public Response updateRegisteredDeviceInfo(@PathParam("deviceId") String deviceId,
+//                                               DeviceRegistrationType deviceRegistrationType) {
+//        UpdateDeviceRegistrationCommand updateDeviceRegistrationCommand = new UpdateDeviceRegistrationCommand(
+//                deviceId,
+//                deviceRegistrationType.getDeviceId(),
+//                deviceRegistrationType.getDeviceType(),
+//                deviceRegistrationType.getDeviceUri(),
+//                deviceRegistrationType.getStatusUpdatePeriod(),
+//                deviceRegistrationType.getDeviceTags());
+//        DeviceRegistrationType updated = toDeviceRegistrationTypeMapper
+//                .map(updateDeviceRegistrationUseCase.updateDeviceRegistration(updateDeviceRegistrationCommand), uriInfo);
+//        return Response.ok().entity(updated).build();
+//    }
+//
+//    @DELETE
+//    @Path("{deviceId}")
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public Response deleteRegisteredDevice(@PathParam("deviceId") String deviceId) {
+//        deleteReisteredDevicesUseCase.deleteRegisteredDevice(new DeleteRegisteredDeviceCommand(deviceId));
+//        return Response.ok().build();
+//    }
 }
